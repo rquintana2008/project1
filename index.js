7dfc39b1bd5397fd5d4c8acd1deaae543bff525d
rquintana2008 <royce.quintana7q@gmail.com>
2014-06-12 23:11:09 -0600

    completed application

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start; 
 handle["/upload"] = requestHandlers.upload; 
+handle["/show"] = requestHandlers.show; 
 
 server.start(router.route, handle); 
